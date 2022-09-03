69879a71c29922a83c69b53f7439819c734ddaed
Sardius <0xsardius@gmail.com>
2022-09-03 09:44:57 -0400

    first commit

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,14 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: networks.config.blockConfirmations || 1,
+    })
+}
